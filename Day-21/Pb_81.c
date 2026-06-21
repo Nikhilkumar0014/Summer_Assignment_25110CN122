bd7d5b219c96affd20b03b33fd92fbb582e1e230
Nikhilkumar0014 <gautamnikhil1980@gmail.com>
2026-06-21 14:11:40 +0530

    solved all problems of day 21

---
+++ Day-21/Pb_81.c
@@ -0,0 +1,13 @@
+//Q81.
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0;
+    printf("enter a string:");
+    scanf("%s",str);
+    while(str[i]!='\0')
+       i++;
+    printf("length=%d",i);
+    return 0;
+}
---
+++ Day-21/Pb_82.c
@@ -0,0 +1,14 @@
+//Q82.
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,len=0;
+    printf("enter a string:");
+    scanf("%s",str);
+    while(str[len]!='\0')
+      len++;
+    for(i=len-1;i>=0;i--)
+      printf("%c",str[i]);
+    return 0;
+}
